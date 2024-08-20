edc3cfb468d4f6357b59e94b3522d64c61b7e099
RITHIKA ANILKUMAR <83855321+Rithika-Anilkumar@users.noreply.github.com>
2024-08-20 20:51:48 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import AppRouter from './components/AppRouter/AppRouter';
+
+const App = () => {
+  return <AppRouter/>
+  
+}
+
+export default App
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import { CircularProgress } from "@mui/material"
+import "./Loader.css";
+function Loader() {
+  return (
+    <div className ="Loader">
+        <CircularProgress />
+    </div>
+  )
+}
+
+export default Loader
